fc07537feadff560f1e00429f3a3a341fcb2ebeb
ltrel <79512656+ltrel@users.noreply.github.com>
2023-08-05 21:47:50 +1000

    Clean up code

--- src/TradeHistory.tsx
+++ src/TradeHistory.tsx
@@ -1,28 +1,10 @@
-import { z } from "zod";
-import { parse, stringify } from "csv/browser/esm/sync";
+import { stringify } from "csv/browser/esm/sync";
 import { DataGrid, GridColDef, GridEventListener, GridRowEditStartParams, GridRowEditStopParams, GridRowEditStopReasons, GridRowModel, GridRowsProp, MuiEvent, useGridApiRef } from "@mui/x-data-grid";
 import { useState } from "react";
 import UploadButton from "./UploadButton";
 import { Box, Button, Container, Stack } from '@mui/material';
 import { downloadStr, formatLocalYMD } from './util';
-
-interface TradeRecord {
-  date: Date;
-  orderType: 'Buy' | 'Sell';
-  sym: string,
-  unitPrice: number,
-  quantity: number,
-  fees: number,
-}
-const TradeRecordSchema = z.object({
-  date: z.date(),
-  orderType: z.enum(['Buy', 'Sell']),
-  sym: z.string(),
-  unitPrice: z.number(),
-  quantity: z.number(),
-  fees: z.number(),
-})
-const TradeArraySchema = z.array(TradeRecordSchema);
+import { TradeRecord, TradeRecordSchema, tradesFromCSV } from "./data/trade";
 
 const initialTrades: TradeRecord[] = [
   {
@@ -48,26 +30,6 @@ export default function TradeHistory() {
   const [trades, setTrades] = useState(initialTrades);
   const [showNewRow, setShowNewRow] = useState(false);
 
-  const columns: GridColDef[] = [
-    { flex: 1, field: 'date', headerName: 'Date', type: 'date', editable: true },
-    { flex: 1, field: 'orderType', headerName: 'Order Type', editable: true },
-    { flex: 1, field: 'sym', headerName: 'Symbol', editable: true },
-    { flex: 1, field: 'unitPrice', headerName: 'Unit Price', type: 'number', editable: true },
-    { flex: 1, field: 'quantity', headerName: 'Quantity', type: 'number', editable: true },
-    { flex: 1, field: 'fees', headerName: 'Fees', type: 'number', editable: true },
-    { flex: 1, field: 'total', headerName: 'Total', type: 'number' },
-  ]
-
-  let rows: GridRowsProp = trades.map((x, i) => ({
-    ...x,
-    id: i,
-    total: x.unitPrice * x.quantity * (x.orderType === 'Buy' ? -1 : 1) - x.fees
-  }));
-  if (showNewRow) {
-    const newId = rows.length;
-    rows = [...rows, {id: newId}];
-  }
-
   const enterEditMode = () => {
     setShowNewRow(true);
     apiRef.current.startRowEditMode({id: trades.length});
@@ -79,23 +41,7 @@ export default function TradeHistory() {
 
   const handleImport = async (file: File) => {
     try {
-      const text = await file.text();
-      const newTrades: unknown = parse(text, {
-        columns: true,
-        cast: (value, context) => {
-          if (context.header) {
-            return value;
-          }
-          else if (context.column === 'date') {
-            return new Date(value);
-          }
-          else if (context.index > 2) {
-            return Number(value);
-          }
-          return value;
-        },
-      });
-      setTrades(TradeArraySchema.parse(newTrades));
+      setTrades(await tradesFromCSV(file));
     }
     catch(e) {
       alert('Error: file could not be imported')
@@ -128,6 +74,26 @@ export default function TradeHistory() {
     exitEditMode();
     return newRow;
   };
+
+  const columns: GridColDef[] = [
+    { flex: 1, field: 'date', headerName: 'Date', type: 'date', editable: true },
+    { flex: 1, field: 'orderType', headerName: 'Order Type', editable: true },
+    { flex: 1, field: 'sym', headerName: 'Symbol', editable: true },
+    { flex: 1, field: 'unitPrice', headerName: 'Unit Price', type: 'number', editable: true },
+    { flex: 1, field: 'quantity', headerName: 'Quantity', type: 'number', editable: true },
+    { flex: 1, field: 'fees', headerName: 'Fees', type: 'number', editable: true },
+    { flex: 1, field: 'total', headerName: 'Total', type: 'number' },
+  ]
+
+  let rows: GridRowsProp = trades.map((x, i) => ({
+    ...x,
+    id: i,
+    total: x.unitPrice * x.quantity * (x.orderType === 'Buy' ? -1 : 1) - x.fees
+  }));
+  if (showNewRow) {
+    const newId = rows.length;
+    rows = [...rows, {id: newId}];
+  }
 
   let addOrCancelButton;
   if (showNewRow) {
---
+++ src/data/trade.ts
@@ -0,0 +1,42 @@
+import { z } from "zod";
+import { parse, stringify } from "csv/browser/esm/sync";
+
+export interface TradeRecord {
+  date: Date;
+  orderType: 'Buy' | 'Sell';
+  sym: string,
+  unitPrice: number,
+  quantity: number,
+  fees: number,
+}
+
+export const TradeRecordSchema = z.object({
+  date: z.date(),
+  orderType: z.enum(['Buy', 'Sell']),
+  sym: z.string(),
+  unitPrice: z.number(),
+  quantity: z.number(),
+  fees: z.number(),
+})
+
+export const TradeArraySchema = z.array(TradeRecordSchema);
+
+export const tradesFromCSV = async (file: File): Promise<TradeRecord[]> => {
+  const text = await file.text();
+  const newTrades: unknown = parse(text, {
+    columns: true,
+    cast: (value, context) => {
+      if (context.header) {
+        return value;
+      }
+      else if (context.column === 'date') {
+        return new Date(value);
+      }
+      else if (context.index > 2) {
+        return Number(value);
+      }
+      return value;
+    },
+  });
+  return TradeArraySchema.parse(newTrades);
+}
